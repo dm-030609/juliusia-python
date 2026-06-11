363031b08c508f83cd58b937f9e1f3833fafd68c
dm-030609 <contatomcn.labs@gmail.com>
2026-06-11 00:27:17 -0300

    feat: implementa classe Gasto com type hints

--- src/julius/gasto.py
+++ src/julius/gasto.py
@@ -1,2 +1,15 @@
 class Gasto:
-    pass
+    def __init__(self, data: str, categoria: str, valor: float, descricao: str):
+        self.data = data
+        self.categoria = categoria
+        self.valor = valor
+        self.descricao = descricao
+
+
+    def __str__(self):
+        return f"{self.data} | {self.categoria} | R${self.valor:.2f} | {self.descricao}"
+
+
+    def __repr__(self):
+        return f"Gasto(data='{self.data}', valor={self.valor})"
+
