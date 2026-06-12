3cd5122988ddcaff043ac0ff0e288b416511a950
dm-030609 <contatomcn.labs@gmail.com>
2026-06-12 01:28:26 -0300

    Feat: API Fast com 4 rotas implementada

---
+++ src/julius/api.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from julius.gasto import Gasto
+from julius.registro import RegistroFinanceiro
+from julius.relatorio import RelatorioFinanceiro
+from julius.sanitizador import SanitizadorBR
+from pydantic import BaseModel
+
+app = FastAPI()
+registro = RegistroFinanceiro()
+relatorio = RelatorioFinanceiro(registro)
+
+class GastoInput(BaseModel):
+    descricao: str
+    valor: str
+    categoria: str
+    data: str
+
+
+@app.post("/gastos")
+def criar_gasto(entrada: GastoInput):
+    valor_limpo = SanitizadorBR.sanitizar_valor(entrada.valor)
+    gasto = Gasto(entrada.data, entrada.categoria, valor_limpo, entrada.descricao)
+    registro.adicionar(gasto)
+
+    return {"mensagem": "Gasto adicionado", "total": registro.total()}
+
+
+@app.get("/gastos")
+def listar_gastos():
+    return {"gastos": [str(g) for g in  registro._gastos], "total": registro.total()}
+
+@app.get("/relatorio/categorias")
+def relatorio_categorias():
+    return {"gastos": relatorio.gerar_por_categoria()}
+
+@app.get("/relatorio/mensal/{mes}/{ano}")
+def relatorio_mensal(mes: str, ano: str):
+    return {"gastos": relatorio.resumo_mensal(f"{mes}/{ano}")}
